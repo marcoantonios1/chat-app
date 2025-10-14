49650786503a34c27c61335c941847444d737a53
Marco <marco_antonios1@outlook.com>
2025-10-14 16:33:01 +0300

    init cli

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,24 @@
 package client
+
+import (
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+// main entry point for the temporal server
+func main() {
+	app := buildCLI()
+	_ = app.Run(os.Args)
+}
+
+func buildCLI() *cli.App {
+	app := cli.NewApp()
+	app.Name = "chatapp"
+	app.Usage = "Client for chatapp"
+	app.Version = "0.1.0"
+	app.ArgsUsage = " "
+	app.Flags = []cli.Flag{}
+	app.Commands = []cli.Command{}
+	return app
+}
